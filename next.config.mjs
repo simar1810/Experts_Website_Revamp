035034f8e1360e192eeadc85587514a77e91b028
RaghavArora2 <raghavarora419@gmail.com>
2026-04-28 14:10:30 +0530

    bug 1

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         hostname: "**.amazonaws.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/**",
+      },
     ],
   },
 };
